aaa7f229d50cbafd921f5379e6d65db4cf33c196
kranthi09 <g.kranthi2507@gmail.com>
2021-09-03 19:07:04 +0530

    alter models

--- essentails/models.py
+++ essentails/models.py
@@ -1,7 +1,27 @@
 from django.db import models
 from django.db.models.base import Model
+from django.db.models.enums import Choices
 
 # Create your models here.
+
+
+class Form(models.Model):
+    FORM_STATUS = [
+        ('LIVE', 'Live'),
+        ('CLOSED', 'Closed'),
+        ('DONE', 'Done')
+    ]
+    name = models.CharField(max_length=200)
+    status = models.CharField(
+        choices=FORM_STATUS,
+        default=None,
+        max_length=20
+    )
+    updates = models.TextField(
+        default="description_form",
+        null=True
+    )
+
 
 class Brand(models.Model):
     name = models.CharField(max_length=200)
@@ -10,7 +30,6 @@ class Brand(models.Model):
         null=True
     )
     is_available = models.BooleanField(default=None)
-
 
 class Item(models.Model):
     ITEM_CATEGORIES = [
@@ -25,35 +44,12 @@ class Item(models.Model):
         default='Snacks',
         max_length=20
     )
-    brands = models.ManyToManyField(
-        Brand,
-        through='ItemBrand',
-        through_fields=('item', 'brand')
-    )
-    is_available = models.BooleanField(default=None)
-
-
-class ItemBrand(models.Model):
-    item = models.ForeignKey(Item, on_delete=models.CASCADE)
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    form = models.ForeignKey(Form, on_delete=models.CASCADE)
+    is_available = models.BooleanField(default=None)
     quantity = models.IntegerField(default=0)
     price = models.DecimalField(
         default=0,
         decimal_places=3,
         max_digits=9
     )
-    
-
-
-class Form(models.Model):
-    name = models.CharField(max_length=200)
-    status = models.BooleanField(default=None)
-    updates = models.TextField(
-        default="description_form",
-        null=True
-    )
-    is_available = models.BooleanField(default=None)
-
-class FormItem(models.Model):
-    form = models.ForeignKey(Form, on_delete=models.CASCADE)
-    item = models.ForeignKey(Item, on_delete=models.CASCADE)
--- populate.py
+++ populate.py
@@ -2,5 +2,41 @@
 # 1. Create five brands
 # 2. Create 25 items
 # 3. add 5 items to each brand
-# 4. Create 4 Forms
+
 # 4. add 6 items in each form
+
+# Create 5 Brands
+
+from essentails.models import Brand, Form, Item
+
+
+brands = Brand.objects.bulk_create(
+    Brand(name="Nestle", is_available=True),
+    Brand(name="Britania", is_available=True),
+    Brand(name="Cadbury", is_available=True),
+    Brand(name="Unibic", is_available=True),
+)
+# 4. Create 4 Forms - Done
+snack_form = Form(name="Snacks Form", status="Live")
+fruits_form = Form(name="Fruits Form", status="Live")
+cloths_form = Form(name="Cloths Form", status="Live")
+accom_form = Form(name="Accomidation Kit Form", status="Live")
+
+# For Snacks form Create 5 items
+
+spinash_chips = Item(name="Potato Chips - Spanish Tomato Tango", category="Snacks"),
+american_style = Item(name="Potato Chips -  American Style Cream & Onion", category="Snacks"),
+mad_angles = Item(name="MAD Angels", category="Snacks"),
+rusks = Item(name="Haldiram Rusk", category="Snacks"),
+halke_fulke = Item(name="Halke fulke", category="Snacks")
+
+# for each snack item create 3 brands
+lays = Brand.objects.create(name="Lays")
+bingo = Brand.objects.create(name="Bingo")
+haldirams = Brand.objects.create(name="Haldirams")
+
+lays.items.add(spinash_chips, american_style)
+bingo.items.add(mad_angles)
+haldirams.items.add(rusks, halke_fulke)
+
+snack_form.items.add(spinash_chips, american_style, mad_angles, rusks, halke_fulke)
