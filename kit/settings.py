142b7780859709a104928de847f25f2b015a75e5
skywalker <g.kranthi2507@gmail.com>
2021-09-05 13:31:23 +0530

    commit

--- kit/settings.py
+++ kit/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'essentails.apps.EssentailsConfig',
+    'essentails',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
